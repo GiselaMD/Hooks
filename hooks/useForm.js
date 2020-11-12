b7effa6076985deb2774b149a22fddbaf0ea9de7
Gisela Difini <gdifini@degdigital.com>
2020-11-12 09:04:30 -0300

    useform and useonclickoutside hooks

---
+++ hooks/useForm.js
@@ -0,0 +1,15 @@
+export const useForm = initialValues => {
+  const [values, setValues] = useState(initialValues);
+
+  return [
+    values,
+    e => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    },
+    // reset
+    () => setValues(initialValues)
+  ];
+};
